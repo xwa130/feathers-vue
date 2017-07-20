cc638295004c5f794bf6f0d5f10e748502a9d297
xwa130 <xwa130@sfu.ca>
2017-07-20 10:39:55 -0700

    refactor code

--- vue-feathers.js
+++ vue-feathers.js
@@ -1,5 +1,5 @@
 (function() {
-  'use strict'
+  'use strict';
 
   var plugin = {
     install: function(Vue, server) {
@@ -7,50 +7,48 @@
       // Every component will have this
       Vue.mixin({
         created: function() {
-          this.$feathers = server
-
+          // Setting up the client
+          this.$feathers = server;
           // Setting up the services
-          this.$services = server.services
+          this.$services = server.services;
 
-          if(!this.$options.feathers)
-            return
+          var feathers = this.$options.feathers;
+          if(!feathers)
+            return;
 
           // Setting up the events
-          var keys = Object.keys(this.$options.feathers) // services, IE9+
-          for(var i = 0; i < keys.length; i++) {
-            var service = this.$options.feathers[keys[i]]
-            var ekeys = Object.keys(service) // handlers
-
-            for(var k = 0; k < ekeys.length; k++) {
-              server.service(keys[i]).on(ekeys[k], service[ekeys[k]].bind(this))
-            }
-          }
+          Object.keys(feathers).forEach(function(serviceKey) {
+            var service = feathers[serviceKey];
+            Object.keys(service).forEach(function(eventKey) {
+              server.service(serviceKey)
+                .on(eventKey, service[eventKey].bind(this));
+            });
+          });
         },
 
         beforeDestroy: function() {
+          var feathers = this.$options.feathers;
+          if(!this.$options || !feathers)
+            return;
+
           // Removing the events
-          if(!this.$options || !this.$options.feathers)
-            return
-
-          var keys = Object.keys(this.$options.feathers) // services, IE9+
-          for(var i = 0; i < keys.length; i++) {
-            var service = this.$options.feathers[keys[i]]
-            var ekeys = Object.keys(service) // handlers
-
-            for(var k = 0; k < ekeys.length; k++) {
-              server.service(keys[i]).removeListener(ekeys[k], service[ekeys[k]].bind(this))
-            }
-          }
+          Object.keys(feathers).forEach(function(serviceKey) {
+            var service = feathers[serviceKey];
+            Object.keys(service).forEach(function(eventKey) {
+              server.service(serviceKey)
+                .removeListener(eventKey, service[eventKey].bind(this));
+            });
+          });
         }
-      })
+      });
     }
-  }
+  };
 
   if(typeof exports === 'object' && typeof module === 'object') {
-    module.exports = plugin
+    module.exports = plugin;
   } else if(typeof define === 'function' && define.amd) {
-    define(function () { return plugin })
+    define(function () { return plugin; });
   } else if (typeof window !== 'undefined') {
-    window.VueAsyncData = plugin
+    window.VueAsyncData = plugin;
   }
-})()
+})();
