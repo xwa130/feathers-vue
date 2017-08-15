06fc9c5a681f6e4b385466c0c76dfaae304f49c7
xwa130 <xwa130@sfu.ca>
2017-08-15 11:40:04 -0700

    conform to ES6 syntax

--- src/index.js
+++ src/index.js
@@ -1,40 +1,48 @@
 'use strict';
 
-var plugin = {
-  install: function(Vue, server) {
+const plugin = {
+  install(Vue, server) {
     // Every component will have this
     Vue.mixin({
-      created: function() {
+      created() {
         // Setting up the client
         this.$feathers = server;
         // Setting up the services
         this.$services = server.services;
 
-        var feathers = this.$options.feathers;
+        const feathers = this.$options.feathers;
         if(!feathers)
           return;
 
         // Setting up the events
         Object.keys(feathers).forEach(function(serviceKey) {
-          var service = feathers[serviceKey];
+          const service = feathers[serviceKey];
           Object.keys(service).forEach(function(eventKey) {
-            server.service(serviceKey)
-              .on(eventKey, service[eventKey].bind(this));
+            server
+              .service(serviceKey)
+              .on(
+                eventKey,
+                service[eventKey].bind(this)
+              );
           });
         });
       },
 
-      beforeDestroy: function() {
-        var feathers = this.$options.feathers;
+      beforeDestroy() {
+        const feathers = this.$options.feathers;
         if(!this.$options || !feathers)
           return;
 
         // Removing the events
         Object.keys(feathers).forEach(function(serviceKey) {
-          var service = feathers[serviceKey];
+          const service = feathers[serviceKey];
           Object.keys(service).forEach(function(eventKey) {
-            server.service(serviceKey)
-              .removeListener(eventKey, service[eventKey].bind(this));
+            server
+              .service(serviceKey)
+              .removeListener(
+                eventKey,
+                service[eventKey].bind(this)
+              );
           });
         });
       },
@@ -45,7 +53,7 @@ var plugin = {
 if(typeof exports === 'object' && typeof module === 'object') {
   module.exports = plugin;
 } else if(typeof define === 'function' && define.amd) {
-  define(function () { return plugin; });
+  define(() => plugin);
 } else if (typeof window !== 'undefined') {
   window.VueAsyncData = plugin;
 }
